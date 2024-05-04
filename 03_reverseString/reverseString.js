0c067edfb8a67c13d0b858219225b087b71f74db
Alex Bandis <bantalex3@gmail.com>
2024-05-04 20:48:28 +0300

    Finish task 03(With .reverse()

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let newStr = str.split('')
 
+    newStr.reverse();
+    
+  
+
+    return newStr.join('');
 };
+
+console.log(reverseString('hoi'))
 
 // Do not edit below this line
 module.exports = reverseString;
