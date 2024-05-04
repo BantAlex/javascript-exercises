c1e9b467653e2a5073ec53074ceae250e1dd792e
Alex Bandis <bantalex3@gmail.com>
2024-05-04 20:24:33 +0300

    Finish task 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+    if (num < 0) return "ERROR"
+    let newStr = ''
+    for (let i = 0; i < num; i++){
+        newStr = newStr + string;
+    }
+    return newStr;
 
 };
+
+console.log(repeatString('good', -1))
 
 // Do not edit below this line
 module.exports = repeatString;
