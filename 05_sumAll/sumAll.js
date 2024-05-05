c673571041d40f223eba149d6d85782ff67d240a
Alex Bandis <bantalex3@gmail.com>
2024-05-05 19:45:01 +0300

    Finish task 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function(first,last) {
+    let result = 0;
 
+    if (first !== parseInt(first) || last !== parseInt(last) || first < 0 || last < 0){
+        return "ERROR";
+    };
+    if (first < last){
+        for (let i = first; i <= last; i++){
+            result+= i;
+        } 
+    } else if (first > last){
+        for (let i = last; i <= first; i++){
+            result += i;
+        }
+    } else {
+        return first;
+    }
+    
+    
+
+    return result;
 };
 
+
+console.log(sumAll(4,1));
 // Do not edit below this line
 module.exports = sumAll;
