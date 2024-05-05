c092f6f0393cb5d872c9b7b06b45e0a71a904cf1
Alex Bandis <bantalex3@gmail.com>
2024-05-05 19:18:37 +0300

    Finish task 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
 
+    // return arr.includes(2)
+    //return arr.filter((remove) => remove != args[i]);
+    let newArr = []; 
+    for (let i = 0; i < arr.length; i++){
+        if (!args.includes(arr[i])){
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
+    
 };
 
+console.log(removeFromArray([0,2,6,7,3,2,1,2,2,2,2,2],2,7,3,1,0,6))
 // Do not edit below this line
 module.exports = removeFromArray;
