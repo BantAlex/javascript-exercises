0a1822b5b362dbe1197ffc1e509c7d9cce9117e8
Alex Bandis <bantalex3@gmail.com>
2024-05-05 20:07:13 +0300

    Finish task 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let result = (f-32) * (5/9)
+  return Math.round(result * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let result = ((9/5) * c) + 32
+  return Math.round(result * 10) / 10 
 };
+
+console.log(convertToCelsius(100));
+console.log(convertToFahrenheit(0)); 
 
 // Do not edit below this line
 module.exports = {
