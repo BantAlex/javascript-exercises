19db67a0ffeab2a1cb499c760ace8ff25269b403
Alex Bandis <bantalex3@gmail.com>
2024-05-11 20:46:46 +0300

    Finish task 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let regex = /[^a-zA-Z0-9]/
+    let reversed = str.split('').reverse().filter(char => !regex.test(char) ).join('');
+    let actualString = str.split('').filter(char => !regex.test(char) ).join('');
+    return reversed.toLowerCase() === actualString.toLowerCase();
 };
 
 // Do not edit below this line
