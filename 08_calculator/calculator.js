72ff79815da581c0f09a8765acfebcbe92cf3f84
Alex Bandis <bantalex3@gmail.com>
2024-05-11 17:40:49 +0300

    Finish task 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(n1,n2) {
+	return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1,n2) {
+	return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length === 0){
+    return 0;
+  } else {
+    return arr.reduce((n1,n2)=>n1+n2);
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((n1,n2) => n1 * n2 );
 };
 
-const power = function() {
-	
+const power = function(n1,n2) {
+  return Math.pow(n1,n2)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let arr2 = [1];
+  for (let i = 1; i <= num; i++){
+    arr2.push(i);
+  }
+    return multiply(arr2)
 };
 
 // Do not edit below this line
