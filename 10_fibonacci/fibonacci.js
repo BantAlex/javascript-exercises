9d3bd8de2e6249727d7f175ff71e3a8f768309ee
Alex Bandis <bantalex3@gmail.com>
2024-05-11 22:00:01 +0300

    Could not manage that one

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,33 @@
-const fibonacci = function() {
+const fibonacci = function(fibNum) {
+    let count
+    if (typeof fibNum !== 'number') {
+        count = parseInt(fibNum)
+    } else {
+        count = fibNum
+    }
+
+    if (count < 0) return "OOPS";
+    if (count == 0) return 0;
+
+    let firstPrev = 1;
+    let secondPrev = 0;
+
+    for (let i = 2; i <= count; i++) {
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+
+    return firstPrev;
 
 };
 
+
+
+console.log(fibonacci(-4));
+
 // Do not edit below this line
 module.exports = fibonacci;
+
+// In this exercise, the Fibonacci sequence used is 1, 1, 2, 3, 5, 8, etc.
+//A series of numbers in which each number ( Fibonacci number ) is the sum of the two preceding numbers.
