849ad207af8adb6b8ded8017a7bb1c25123fdf3c
Alex Bandis <bantalex3@gmail.com>
2024-05-12 23:01:01 +0300

    Didn't solve task 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(obj) {
+    if (!obj[0].yearOfDeath){
+        return obj.sort((a,b)=> (a.yearOfBirth - b.yearOfBirth) - (a.yearOfDeath - b.yearOfDeath))[1];
+    } else {
+        return obj.sort((a,b)=> (a.yearOfBirth - b.yearOfBirth) - (a.yearOfDeath - b.yearOfDeath))[0];
+    }
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1066,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+console.log(people[0].yearOfDeath)
